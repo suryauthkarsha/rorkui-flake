9b8f989a5c0c147430741822cc5ebe9cfcb7724f
Surya Uthkarsha <suryauthkarsha@gmail.com>
2026-04-28 10:51:43 +0530

    Initial commit from Flake AI

---
+++ app/sign-up/page.tsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default function Signup() {
+  return (
+    <div className="bg-[rgb(252,252,252)] text-[rgb(37,39,40)] text-[16px] leading-[24px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:640px_400px] relative mx-auto overflow-hidden min-h-screen box-border w-[1280px] h-[800px]">
+      <div className="bg-[rgb(252,252,252)] text-[rgb(37,39,40)] text-[16px] leading-[24px] p-[16px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:640px_400px] absolute left-[0px] top-[0px] box-border w-[1280px] h-[800px]">
+        <div className="text-[rgb(37,39,40)] text-[16px] leading-[24px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:170px_284.25px] absolute left-[470px] top-[115.75px] box-border w-[340px] h-[568.5px]">
+          <div className="text-[rgb(37,39,40)] text-[16px] leading-[24px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:170px_18px] absolute left-[0px] top-[0px] box-border w-[340px] h-[36px]">
+            <Link href="/" className="text-[rgb(37,39,40)] text-[16px] leading-[24px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:18px_18px] absolute left-[152px] top-[0px] box-border w-[36px] h-[36px]">
+              <div dangerouslySetInnerHTML={{ __html: `<svg viewBox="0 0 300 300" fill="currentColor" class="size-9"><path d="M253.339 0H46.4123C20.7798 0 0 21.304 0 47.7159V107.267C0 111.503 2.98631 114.12 7.09249 112.874L238.283 52.6993C244.256 51.0797 247.74 55.6894 245.998 61.6694L185.649 292.649C184.529 297.135 187.267 300 191.746 300H252.095C278.1 300 300 277.699 300 252.409V47.7159C300 21.304 279.096 0 253.339 0Z"></path><path d="M0 139.531V253.526C0 278.942 20.6553 299.996 46.4123 299.996H159.768C164.247 299.996 166.362 296.259 165.118 291.898L147.822 229.232C147.076 226.865 147.2 224.747 148.445 222.38L203.691 111.251C206.802 105.021 199.336 100.412 194.857 105.146L53.8781 244.93C49.8963 249.166 43.5504 244.307 45.7901 238.701L72.1692 160.212C73.538 155.727 71.6715 151.367 66.321 150.37L7.71464 134.299C3.11074 132.679 0 135.171 0 139.531Z"></path></svg>` }}  className="text-[rgb(37,39,40)] text-[16px] leading-[24px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:18px_18px] absolute left-[0px] top-[0px] box-border w-[36px] h-[36px]" />
+            </Link>
+          </div>
+          <div className="bg-[rgb(247,247,247)] text-[rgb(37,39,40)] text-[16px] leading-[24px] p-[20px] rounded-[4px] border-[0px_solid_rgb(235,_235,_235)] shadow-[rgba(0,_0,_0,_0)_0px_0px_0px_0px,_rgba(0,_0,_0,_0)_0px_0px_0px_0px,_rgba(0,_0,_0,_0)_0px_0px_0px_0px,_rgb(235,_235,_235)_0px_0px_0px_1px,_rgba(0,_0,_0,_0)_0px_0px_0px_0px] [transform-origin:170px_232.5px] absolute left-[0px] top-[68px] box-border w-[340px] h-[465px]">
+            <p className="text-[rgb(37,39,40)] text-[15px] font-[600] leading-[22.5px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:150px_11.25px] absolute left-[20px] top-[20px] box-border w-[300px] h-[22.5px] overflow-hidden text-clip">
+              <span className="text-[rgb(37,39,40)] text-[15px] font-[600] leading-[22.5px] text-center [transform-origin:150px_11.25px] absolute left-[76.984375px] top-[1px] box-border w-[146.015625px] h-[19px] overflow-hidden text-clip block">
+                Create your account
+              </span>
+            </p>
+            <p className="text-[rgb(155,157,158)] text-[13px] leading-[19.5px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:150px_9.75px] absolute left-[20px] top-[46.5px] box-border w-[300px] h-[19.5px] overflow-hidden text-clip">
+              <span className="text-[rgb(155,157,158)] text-[13px] leading-[19.5px] text-center [transform-origin:150px_9.75px] absolute left-[78.546875px] top-[1px] box-border w-[142.90625px] h-[17px] overflow-hidden text-clip block">
+                Get started with Median
+              </span>
+            </p>
+            <div className="text-[rgb(37,39,40)] text-[16px] leading-[24px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:150px_18px] absolute left-[20px] top-[86px] box-border w-[300px] h-[36px]">
+              <button className="bg-[rgb(252,252,252)] text-[rgb(37,39,40)] text-[13px] font-[500] leading-[19.5px] rounded-[4px] border-[0px_solid_rgb(235,_235,_235)] shadow-[rgba(0,_0,_0,_0)_0px_0px_0px_0px,_rgba(0,_0,_0,_0)_0px_0px_0px_0px,_rgba(0,_0,_0,_0)_0px_0px_0px_0px,_rgb(235,_235,_235)_0px_0px_0px_1px,_rgba(0,_0,_0,_0)_0px_0px_0px_0px] [transform-origin:73px_18px] absolute left-[0px] top-[0px] box-border w-[146px] h-[36px]">
+                <div dangerouslySetInnerHTML={{ __html: `<svg class="size-4" viewBox="0 0 24 24" fill="currentColor"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"></path></svg>` }}  className="text-[rgb(37,39,40)] text-[13px] font-[500] leading-[19.5px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:8px_8px] absolute left-[65px] top-[10px] box-border w-[16px] h-[16px]" />
+              </button>
+              <button className="bg-[rgb(252,252,252)] text-[rgb(37,39,40)] text-[13px] font-[500] leading-[19.5px] rounded-[4px] border-[0px_solid_rgb(235,_235,_235)] shadow-[rgba(0,_0,_0,_0)_0px_0px_0px_0px,_rgba(0,_0,_0,_0)_0px_0px_0px_0px,_rgba(0,_0,_0,_0)_0px_0px_0px_0px,_rgb(235,_235,_235)_0px_0px_0px_1px,_rgba(0,_0,_0,_0)_0px_0px_0px_0px] [transform-origin:73px_18px] absolute left-[154px] top-[0px] box-border w-[146px] h-[36px]">
+                <div dangerouslySetInnerHTML={{ __html: `<svg class="size-4" viewBox="0 0 24 24"><path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92a5.06 5.06 0 0 1-2.2 3.32v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.1z" fill="#4285F4"></path><path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"></path><path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05"></path><path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"></path></svg>` }}  className="text-[rgb(37,39,40)] text-[13px] font-[500] leading-[19.5px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:8px_8px] absolute left-[65px] top-[10px] box-border w-[16px] h-[16px]" />
+              </button>
+            </div>
+            <div className="text-[rgb(37,39,40)] text-[16px] leading-[24px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:150px_8.25px] absolute left-[20px] top-[142px] box-border w-[300px] h-[16.5px]">
+              <div className="text-[rgb(37,39,40)] text-[16px] leading-[24px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:150px_8.25px] absolute left-[0px] top-[0px] box-border w-[300px] h-[16.5px]">
+                <div className="text-[rgb(37,39,40)] text-[16px] leading-[24px] [transform-origin:150px_0.5px] absolute left-[0px] top-[7.75px] box-border w-[300px] h-[1px]"></div>
+              </div>
+              <div className="text-[rgb(37,39,40)] text-[16px] leading-[24px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:150px_8.25px] absolute left-[0px] top-[0px] box-border w-[300px] h-[16.5px]">
+                <p className="bg-[rgb(247,247,247)] text-[rgb(155,157,158)] text-[11px] leading-[16.5px] p-[0px_8px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:63.5859px_8.25px] absolute left-[86.40625px] top-[0px] box-border w-[127.171875px] h-[16.5px] overflow-hidden text-clip">
+                  <span className="text-[rgb(155,157,158)] text-[11px] leading-[16.5px] [transform-origin:63.5859px_8.25px] absolute left-[8px] top-[1px] box-border w-[111.171875px] h-[14px] overflow-hidden text-clip block">
+                    or continue with email
+                  </span>
+                </p>
+              </div>
+            </div>
+            <div className="text-[rgb(37,39,40)] text-[16px] leading-[24px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:150px_133.25px] absolute left-[20px] top-[178.5px] box-border w-[300px] h-[266.5px]">
+              <div className="text-[rgb(37,39,40)] text-[16px] leading-[24px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:150px_29.75px] absolute left-[0px] top-[0px] box-border w-[300px] h-[59.5px]">
+                <div className="text-[rgb(37,39,40)] text-[16px] leading-[24px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:73px_29.75px] absolute left-[0px] top-[0px] box-border w-[146px] h-[59.5px]">
+                  <p className="text-[rgb(37,39,40)] text-[13px] font-[500] leading-[19.5px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:73px_9.75px] absolute left-[0px] top-[0px] box-border w-[146px] h-[19.5px] overflow-hidden text-clip">
+                    <span className="text-[rgb(37,39,40)] text-[13px] font-[500] leading-[19.5px] [transform-origin:73px_9.75px] absolute left-[0px] top-[1px] box-border w-[64.375px] h-[17px] overflow-hidden text-clip block">
+                      First name
+                    </span>
+                  </p>
+                  <div className="bg-[rgb(252,252,252)] text-[rgb(37,39,40)] text-[13px] leading-[19.5px] p-[0px_12px] rounded-[4px] border-[0px_solid_rgb(235,_235,_235)] shadow-[rgba(0,_0,_0,_0)_0px_0px_0px_0px,_rgba(0,_0,_0,_0)_0px_0px_0px_0px,_rgba(0,_0,_0,_0)_0px_0px_0px_0px,_rgb(235,_235,_235)_0px_0px_0px_1px,_rgba(0,_0,_0,_0)_0px_0px_0px_0px] [transform-origin:73px_18px] absolute left-[0px] top-[23.5px] box-border w-[146px] h-[36px]"></div>
+                </div>
+                <div className="text-[rgb(37,39,40)] text-[16px] leading-[24px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:73px_29.75px] absolute left-[154px] top-[0px] box-border w-[146px] h-[59.5px]">
+                  <p className="text-[rgb(37,39,40)] text-[13px] font-[500] leading-[19.5px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:73px_9.75px] absolute left-[0px] top-[0px] box-border w-[146px] h-[19.5px] overflow-hidden text-clip">
+                    <span className="text-[rgb(37,39,40)] text-[13px] font-[500] leading-[19.5px] [transform-origin:73px_9.75px] absolute left-[0px] top-[1px] box-border w-[63.65625px] h-[17px] overflow-hidden text-clip block">
+                      Last name
+                    </span>
+                  </p>
+                  <div className="bg-[rgb(252,252,252)] text-[rgb(37,39,40)] text-[13px] leading-[19.5px] p-[0px_12px] rounded-[4px] border-[0px_solid_rgb(235,_235,_235)] shadow-[rgba(0,_0,_0,_0)_0px_0px_0px_0px,_rgba(0,_0,_0,_0)_0px_0px_0px_0px,_rgba(0,_0,_0,_0)_0px_0px_0px_0px,_rgb(235,_235,_235)_0px_0px_0px_1px,_rgba(0,_0,_0,_0)_0px_0px_0px_0px] [transform-origin:73px_18px] absolute left-[0px] top-[23.5px] box-border w-[146px] h-[36px]"></div>
+                </div>
+              </div>
+              <div className="text-[rgb(37,39,40)] text-[16px] leading-[24px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:150px_29.75px] absolute left-[0px] top-[71.5px] box-border w-[300px] h-[59.5px]">
+                <p className="text-[rgb(37,39,40)] text-[13px] font-[500] leading-[19.5px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:150px_9.75px] absolute left-[0px] top-[0px] box-border w-[300px] h-[19.5px] overflow-hidden text-clip">
+                  <span className="text-[rgb(37,39,40)] text-[13px] font-[500] leading-[19.5px] [transform-origin:150px_9.75px] absolute left-[0px] top-[1px] box-border w-[33.40625px] h-[17px] overflow-hidden text-clip block">
+                    Email
+                  </span>
+                </p>
+                <div className="bg-[rgb(252,252,252)] text-[rgb(37,39,40)] text-[13px] leading-[19.5px] p-[0px_12px] rounded-[4px] border-[0px_solid_rgb(235,_235,_235)] shadow-[rgba(0,_0,_0,_0)_0px_0px_0px_0px,_rgba(0,_0,_0,_0)_0px_0px_0px_0px,_rgba(0,_0,_0,_0)_0px_0px_0px_0px,_rgb(235,_235,_235)_0px_0px_0px_1px,_rgba(0,_0,_0,_0)_0px_0px_0px_0px] [transform-origin:150px_18px] absolute left-[0px] top-[23.5px] box-border w-[300px] h-[36px]"></div>
+              </div>
+              <div className="text-[rgb(37,39,40)] text-[16px] leading-[24px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:150px_29.75px] absolute left-[0px] top-[143px] box-border w-[300px] h-[59.5px]">
+                <p className="text-[rgb(37,39,40)] text-[13px] font-[500] leading-[19.5px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:150px_9.75px] absolute left-[0px] top-[0px] box-border w-[300px] h-[19.5px] overflow-hidden text-clip">
+                  <span className="text-[rgb(37,39,40)] text-[13px] font-[500] leading-[19.5px] [transform-origin:150px_9.75px] absolute left-[0px] top-[1px] box-border w-[59.53125px] h-[17px] overflow-hidden text-clip block">
+                    Password
+                  </span>
+                </p>
+                <div className="bg-[rgb(252,252,252)] text-[rgb(37,39,40)] text-[13px] leading-[19.5px] p-[0px_12px] rounded-[4px] border-[0px_solid_rgb(235,_235,_235)] shadow-[rgba(0,_0,_0,_0)_0px_0px_0px_0px,_rgba(0,_0,_0,_0)_0px_0px_0px_0px,_rgba(0,_0,_0,_0)_0px_0px_0px_0px,_rgb(235,_235,_235)_0px_0px_0px_1px,_rgba(0,_0,_0,_0)_0px_0px_0px_0px] [transform-origin:150px_18px] absolute left-[0px] top-[23.5px] box-border w-[300px] h-[36px]"></div>
+              </div>
+              <div className="text-[rgb(37,39,40)] text-[16px] leading-[24px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:150px_0px] absolute left-[0px] top-[214.5px] box-border w-[300px] h-[0px]"></div>
+              <button className="bg-[rgb(37,39,40)] text-[rgb(252,252,252)] text-[13px] font-[500] leading-[19.5px] rounded-[4px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:150px_18px] absolute left-[0px] top-[230.5px] box-border w-[300px] h-[36px]">
+                <span className="text-[rgb(252,252,252)] text-[13px] font-[500] leading-[19.5px] text-center [transform-origin:150px_18px] absolute left-[103.359375px] top-[9.25px] box-border w-[93.28125px] h-[17px] overflow-hidden text-clip block">
+                  Create account
+                </span>
+              </button>
+            </div>
+          </div>
+          <p className="text-[rgb(155,157,158)] text-[13px] leading-[19.5px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:170px_9.75px] absolute left-[0px] top-[549px] box-border w-[340px] h-[19.5px] overflow-hidden text-clip">
+            <span className="text-[rgb(155,157,158)] text-[13px] leading-[19.5px] text-center [transform-origin:170px_9.75px] absolute left-[71.140625px] top-[1px] box-border w-[153.09375px] h-[17px] overflow-hidden text-clip block">
+              Already have an account?
+            </span>
+            <Link href="/sign-in" className="text-[rgb(37,39,40)] text-[13px] font-[500] leading-[19.5px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:0px_0px] absolute left-[227.484375px] top-[1px] box-border w-[41.375px] h-[17px]">
+              <Link href="/sign-in" className="text-[rgb(37,39,40)] text-[13px] font-[500] leading-[19.5px] text-center [transform-origin:0px_0px] absolute left-[0px] top-[0px] box-border w-[41.375px] h-[17px] overflow-hidden text-clip block">
+                Sign in
+              </Link>
+            </Link>
+          </p>
+        </div>
+      </div>
+      <div className="text-[rgb(37,39,40)] text-[16px] leading-[24px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:640px_0px] absolute left-[0px] top-[800px] box-border w-[1280px] h-[0px]"></div>
+      <div className="text-[rgb(37,39,40)] text-[16px] leading-[24px] border-[0px_solid_rgb(235,_235,_235)] [transform-origin:640px_0px] absolute left-[0px] top-[800px] box-border w-[1280px] h-[0px]"></div>
+    </div>
+  );
+}
